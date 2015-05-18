0f1b8a9d960ee8bbaa601df1017c9b020c90a787
zzzef <zzzef@yahoo.com>
2015-05-19 01:57:26 +0200

    v2-alpha3d
    Derniere version alpha avant passage en beta1 pour la seconde
    consultation.

--- sql_spatialite/gracethd_4_spatialite.sql
+++ sql_spatialite/gracethd_4_spatialite.sql
@@ -1,13 +1,19 @@
-SELECT AddGeometryColumn('t_tranchee','geom',2154,'LINESTRING','XY');
-SELECT AddGeometryColumn('t_artere','geom',2154,'LINESTRING','XY');
+SELECT AddGeometryColumn('t_cheminement','geom',2154,'LINESTRING','XY');
 SELECT AddGeometryColumn('t_cable','geom',2154,'LINESTRING','XY');
 SELECT AddGeometryColumn('t_noeud','geom',2154,'POINT','XY');
-SELECT AddGeometryColumn('t_zone','geom',2154,'MULTIPOLYGON','XY');
+SELECT AddGeometryColumn('t_adresse','geom',2154,'POINT','XY');
+SELECT AddGeometryColumn('t_znro','geom',2154,'MULTIPOLYGON','XY');
+SELECT AddGeometryColumn('t_zsro','geom',2154,'MULTIPOLYGON','XY');
+SELECT AddGeometryColumn('t_zpbo','geom',2154,'MULTIPOLYGON','XY');
+SELECT AddGeometryColumn('t_zdep','geom',2154,'MULTIPOLYGON','XY');
+SELECT AddGeometryColumn('t_zcoax','geom',2154,'MULTIPOLYGON','XY');
 
-
-SELECT CreateSpatialIndex('t_tranchee', 'geom');
-SELECT CreateSpatialIndex('t_artere', 'geom');
+SELECT CreateSpatialIndex('t_cheminement', 'geom');
 SELECT CreateSpatialIndex('t_cable', 'geom');
 SELECT CreateSpatialIndex('t_noeud', 'geom');
-SELECT CreateSpatialIndex('t_zone', 'geom');
-
+SELECT CreateSpatialIndex('t_adresse', 'geom');
+SELECT CreateSpatialIndex('t_znro', 'geom');
+SELECT CreateSpatialIndex('t_zsro', 'geom');
+SELECT CreateSpatialIndex('t_zpbo', 'geom');
+SELECT CreateSpatialIndex('t_zdep', 'geom');
+SELECT CreateSpatialIndex('t_zcoax', 'geom');
